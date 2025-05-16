3311dcb2e2b7c749143d3ee7b19b4a59fc378998
R56123 <181733084+R56123@users.noreply.github.com>
2025-05-16 22:30:33 +0100

    Testing the commands with Data Folder now moved inside the experiments folder

--- experiments/experiment_1.py
+++ experiments/experiment_1.py
@@ -2,7 +2,6 @@ import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 
-
 from model_utils import load_data, train_model, predict, evaluate_model, plot_regression
 
 # Load Dataset
@@ -16,11 +15,9 @@ print(f"\n Experiment 1: High Learning Rate = {learning_rate}\n")
 weight, bias = train_model(X, y, learning_rate=learning_rate, epochs=epochs, return_history=False)
 
 # Predictions 
-
 y_pred = predict(X, weight, bias)
 
 # Evaluate performance 
-
 mse, rmse = evaluate_model(y, y_pred)
 print(f"\n Final Evaluation:\nMSE = {mse:.2f}, RSME = {rmse:.2f}")
 
